1643fff83839d220b23831511f6bc8cd17e2176d
Mustafa K Hussain <hussainm@gameinfinitystudio.com>
2024-08-05 16:11:39 +0530

    smart project and UV packer settings

--- GenerateLightmapUV.py
+++ GenerateLightmapUV.py
@@ -1,7 +1,9 @@
 import bpy
 import os
+import time
 from bpy.props import StringProperty
 from bpy.types import Operator, Panel
+import struct
 
 class GenLightmapUVsOperator(Operator):
     bl_idname = "object.gen_lightmap_uvs"
@@ -12,41 +14,49 @@ class GenLightmapUVsOperator(Operator):
 
     def execute(self, context):
         # Get the directory
+        bpy.context.scene.unit_settings.scale_length = 0.01
         folder_path = self.directory
+
+        #clear the scene
+        bpy.ops.object.select_all(action='SELECT')
+        bpy.ops.object.delete()
         
         # Iterate through all FBX files in the folder
         for file_name in os.listdir(folder_path):
             if file_name.lower().endswith('.fbx'):
                 file_path = os.path.join(folder_path, file_name)
                 
-                # Import the FBX file
+                #perform a preclean
+                bpy.ops.outliner.orphans_purge(do_local_ids=True, do_linked_ids=True, do_recursive=True)
+
                 bpy.ops.import_scene.fbx(filepath=file_path)
                 
                 # Get the imported objects
                 imported_objects = bpy.context.selected_objects[:]
                 
-                # Make a mesh object the active selection
+                # Make a mesh object the active selection to supply proper context
                 for obj in imported_objects:
                     if obj.type == 'MESH':
                         bpy.context.view_layer.objects.active = obj
                         break
                 
-                # Add UV channel
                 bpy.ops.uv.textools_uv_channel_add()
 
-                bpy.ops.object.mode_set(mode='EDIT')
+                bpy.ops.object.mode_set(mode='EDIT') # switch to edit mode to perform UV operations
                 
-                # Lightmap pack
-                # bpy.ops.uv.lightmap_pack(
-                #     PREF_CONTEXT='SEL_FACES', 
-                #     PREF_PACK_IN_ONE=True, 
-                #     PREF_NEW_UVLAYER=False, 
-                #     PREF_MARGIN_DIV=0.5, 
-                #     PREF_BOX_DIV=24
-                # )
+                bpy.ops.uv.smart_project(angle_limit=1.53589, margin_method='SCALED', rotate_method='AXIS_ALIGNED_Y', island_margin=0.0, area_weight=1.0, correct_aspect=True, scale_to_bounds=False)
 
-                bpy.ops.uv.smart_project()
+                #UV Packer Settings
+                bpy.context.scene.UVPackerProps.uvp_combine = False
+                bpy.context.scene.UVPackerProps.uvp_width = 512
+                bpy.context.scene.UVPackerProps.uvp_height = 512
+                bpy.context.scene.UVPackerProps.uvp_engine = 'OP1'
+                bpy.context.scene.UVPackerProps.uvp_padding = 4
                 bpy.ops.uvpackeroperator.packbtn()
+
+                print('Packing......')
+                time.sleep(5) # time to complete packing
+                print('Packing Complete')
                 
                 bpy.ops.object.mode_set(mode='OBJECT')
 
@@ -60,8 +70,8 @@ class GenLightmapUVsOperator(Operator):
                     obj.select_set(True)
                 bpy.ops.object.delete()
 
-                bpy.ops.outliner.orphans_purge(do_local_ids=True, do_linked_ids=True, do_recursive=True)
 
+        print('Complete!')
         return {'FINISHED'}
 
     def invoke(self, context, event):
